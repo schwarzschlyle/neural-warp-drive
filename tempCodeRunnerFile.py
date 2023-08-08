65667ed4009feace8c3c495dba10acaf2b292fc2
Lyle Kenneth <lylekeng@gmail.com>
2023-08-08 17:55:14 +0800

    finished python script for Alcubierre and preliminary neural learning of spherically-symmetric shape functions

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+
+from deepxde.backend import tf
+import deepxde as dde
+import numpy as np
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import plotly.graph_objects as go
+import sympy as sp
+import scipy.integrate as spi
+from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
